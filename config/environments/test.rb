ed571b4bdc1b47d0b1e2ecfe0a07463f24860915
risaharada <supikateriburu@gmail.com>
2015-08-18 14:35:41 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  #bycrypt'のコスト関数を下げることでテストの速度をあげる
+  ActiveModel::SecurePassword.min_cost = true
 end
