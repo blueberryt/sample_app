49c86ffd91169709bc7b8ba780fc7474c5207942
risaharada <supikateriburu@gmail.com>
2015-08-25 16:22:25 +0900

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  before_create :create_remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
